6772fcb8b797b5bf1cc0b94f62cf4425e2c0e122
Sam Huckaby <samhuckaby@gmail.com>
2021-04-17 19:36:40 -0600

    Remove random consoles I used for debugging

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -13,7 +13,6 @@ export class HomeComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   async ngOnInit() {
-    console.log('INFOSEC WAS HERE');
     // Request the 10 most recent sermons (for users without an account);
     this.http.get('/unsecured/topten/sermons').subscribe(result => {
       this.lastTenSermons = result;
